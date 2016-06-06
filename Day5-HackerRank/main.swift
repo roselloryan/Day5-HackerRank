0e5c983986ab3b304871750093c44572184808a4
Ryan Rosello <roselloryan@gmail.com>
2016-06-06 13:37:19 -0400

    looop practice

---
+++ Day5-HackerRank/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Day5-HackerRank
+
+
+import Foundation
+
+let line = "10"
+
+let lineInt = Int(line)!
+
+for i in 1...10 {
+    
+    print("\(i) x \(lineInt) = \(i * lineInt)")
+}
+
+
